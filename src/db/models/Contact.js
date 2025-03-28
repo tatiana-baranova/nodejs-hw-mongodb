ecc8bb70c42cab8c1ad7f367afa60dfaedd31b65
tetiana_baranova <syniovych.tanya@gmail.com>
2025-03-28 22:38:05 +0200

    modal contact

---
+++ src/db/models/Contact.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const contactsSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    email: String,
+    isFavourite: {
+        type: Boolean,
+        default: false,
+    },
+    contactType: {
+        type: String,
+        required: true,
+        enum: ['work', 'home', 'personal'],
+        default: 'personal',
+    },
+},
+    { timestamps: true },
+);
+
+const ContactCollection = model("contacts", contactsSchema);
+
+export default ContactCollection;
