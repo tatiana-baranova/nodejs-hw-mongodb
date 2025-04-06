f6cb65ae40b16fd238a7afe1d6947702617ec45b
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-06 22:30:27 +0300

    routers

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 import pino from "pino-http";
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getContacts, getContactById } from './services/contacts.js';
+// import { getContacts, getContactById } from './services/contacts.js';
 
 export const setupServer = () => {
     const app = express();
@@ -14,34 +14,6 @@ export const setupServer = () => {
             target: 'pino-pretty'
         }
     }));
-
-    app.get("/contacts", async (req, res) => {
-        const data = await getContacts();
-
-        res.json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data,
-        });
-    });
-
-    app.get("/contacts/:contactId", async (req, res) => {
-        const { contactId } = req.params;
-
-        const data = await getContactById(contactId);
-        if (!data) {
-            return res.status(404).json({
-                status: 404,
-                message: 'Contact not found',
-            });
-        }
-        res.json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data,
-        });
-
-    });
 
     app.use((req, res) => {
         res.status(404).json({
