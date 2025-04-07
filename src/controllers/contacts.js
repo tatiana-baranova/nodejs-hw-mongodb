3f474c73ae91f00d192b6fbc9f29e08d7ba496c5
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-07 22:16:56 +0300

    patch contact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,5 @@
 import createHttpError from "http-errors";
-import { getContacts, getContactById, createContact } from "../services/contacts.js";
+import { getContacts, getContactById, createContact, updateContact } from "../services/contacts.js";
 
 export const getContactsController = async (req, res) => {
     const contacts = await getContacts();
@@ -31,5 +31,21 @@ export const createContactController = async (req, res) => {
         status: 201,
         message: "Successfully created a contact!",
         data,
+    });
+};
+
+export const patchContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
+
+    if (!result) {
+        next(createHttpError(404, "Contact not found"));
+        return;
+    }
+
+    res.json({
+        status: 200,
+        message: "Successfully patched a contact!",
+        data: result.contact,
     });
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,4 +3,16 @@ import {ContactCollection} from '../db/models/Contact.js';
 export const getContacts = () => ContactCollection.find();
 export const getContactById = id => ContactCollection.findOne({ _id: id });
 export const createContact = payload => ContactCollection.create(payload);
-
+export const updateContact = async (_id, payload, options = {}) => {
+    const { upsert = false } = options;
+    const rawResult = await ContactCollection.findOneAndUpdate({ _id }, payload, {
+        new: true,
+        upsert,
+        includeResultMetadata: true,
+    });
+    if (!rawResult || !rawResult.value) return null;
+    return {
+        contact: rawResult.value,
+        isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+    };
+};
