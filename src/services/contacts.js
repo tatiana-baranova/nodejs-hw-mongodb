37f594b6ffdb4de1efbf75f547c9fbb3fce04425
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-07 22:53:21 +0300

    delete contact

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,7 +4,7 @@ export const getContacts = () => ContactCollection.find();
 export const getContactById = id => ContactCollection.findOne({ _id: id });
 export const createContact = payload => ContactCollection.create(payload);
 export const updateContact = async (_id, payload, options = {}) => {
-    const { upsert = false } = options;
+    const { upsert } = options;
     const rawResult = await ContactCollection.findOneAndUpdate({ _id }, payload, {
         new: true,
         upsert,
@@ -16,3 +16,5 @@ export const updateContact = async (_id, payload, options = {}) => {
         isNew: Boolean(rawResult?.lastErrorObject?.upserted),
     };
 };
+
+export const deleteContact = id => ContactCollection.findOneAndDelete({ _id: id });
