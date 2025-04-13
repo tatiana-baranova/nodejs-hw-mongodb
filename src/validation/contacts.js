52a0704e54b00b2baf8e684b91df0a2a063024e3
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-14 00:04:03 +0300

    valid Id

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,5 +5,13 @@ export const createContactSchema = Joi.object({
     phoneNumber: Joi.string().min(3).max(20).required(),
     email: Joi.string().email().required(),
     isFavourite: Joi.boolean(),
-    contactType: Joi.string().valid('work', 'home', 'personal').required()
+    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
+
+export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string().min(3).max(20),
+    email: Joi.string().email(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
 });
