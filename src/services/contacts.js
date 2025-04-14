ab1964c30975d68be16e0dc30d53b4b35166ef51
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-14 22:31:34 +0300

    pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,24 @@
 import {ContactCollection} from '../db/models/Contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getContacts = () => ContactCollection.find();
+export const getContacts = async ({ page, perPage }) => {
+    const limit = perPage;
+    const skip = (page - 1) * perPage;
+
+    const contactsQuery = ContactCollection.find();
+    const contactsCount = await ContactCollection.countDocuments();
+
+    const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+    const paginationData = calculatePaginationData(contacts,contactsCount, perPage, page);
+
+    return paginationData;
+};
+    // => ContactCollection.find();
+
+
+
+
 export const getContactById = id => ContactCollection.findOne({ _id: id });
 export const createContact = payload => ContactCollection.create(payload);
 export const updateContact = async (_id, payload, options = {}) => {
