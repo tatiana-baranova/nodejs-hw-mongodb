317cb052dba36e15cfe07153414dcc2561421f85
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-16 23:09:42 +0300

    login router

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,14 +1,21 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUsersSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
+import { registerUsersSchema, loginUsersSchema } from '../validation/auth.js';
+import { registerUserController, loginUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
+
 
 const router = Router();
 
 router.post('/register',
     validateBody(registerUsersSchema),
     ctrlWrapper(registerUserController),
+);
+
+router.post(
+    '/login',
+    validateBody(loginUsersSchema),
+    ctrlWrapper(loginUserController),
 );
 
 export default router;
