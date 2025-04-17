69a026ee49f91af5760339f9d8e011298d18e795
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-17 23:20:36 +0300

    authenticate

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,9 +4,9 @@ import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from '../middlewares/isValidId.js';
-
+import { authenticate } from '../middlewares/authenticate.js';
 const router = Router();
-
+router.use(authenticate);
 router.get("/", ctrlWrapper( getContactsController));
 router.get("/:contactId",isValidId, ctrlWrapper(getContactByIdController));
 router.post("/", validateBody(createContactSchema), ctrlWrapper(createContactController));
