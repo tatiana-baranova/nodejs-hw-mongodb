7525443a2be61498a634ee72703f630461ff26d9
tetiana_baranova <syniovych.tanya@gmail.com>
2025-04-28 22:50:09 +0300

    fixet

--- src/utils/createDirIfNotExists.js
+++ src/utils/createDirIfNotExists.js
@@ -4,7 +4,7 @@ export const createDirIfNotExists = async (url) => {
         await fs.access(url);
     } catch (err) {
         if (err.code === 'ENOENT') {
-            await fs.mkdir(url, { recursive: true });
+            await fs.mkdir(url);
         }
-    } 
+    }
 };
